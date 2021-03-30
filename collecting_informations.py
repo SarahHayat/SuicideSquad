c19606b36ba2ae42d0f8584623f379a2d88ee1d1
SarahHayat <saraahyt@gmail.com>
2021-03-30 13:40:50 +0200

    Edit cpu

--- collecting_informations.py
+++ collecting_informations.py
@@ -12,15 +12,17 @@ def collecting_cpu_informations(times):
     """
     for x in range(times):
         #user: time spent by normal processes executing in user mode; on Linux this also includes guest time
-        #nice: time spent by niced (prioritized) processes executing in user mode; on Linux this also includes guest_nice time
         #system: time spent by processes executing in kernel mode
         #idle: time spent doing nothing
 
         cpu_times = psutil.cpu_times()
         print(f'Times : {cpu_times}')
 
-        cpu_percent = psutil.cpu_percent(interval=1, percpu=False)
+        cpu_percent = psutil.cpu_percent(interval=1, percpu=True)
         print(f'Percent : {cpu_percent}')
+
+        cpu_times_percent = psutil.cpu_times_percent(percpu=True)
+        print(f'Times Percent : {cpu_times_percent}')
 
         cpu_count = psutil.cpu_count(logical=True)
         print(f'Count : {cpu_count}')
@@ -30,17 +32,11 @@ def collecting_cpu_informations(times):
 
         #ctx_switches: number of context switches (voluntary + involuntary) since boot.
         #interrupts: number of interrupts since boot.
-        #soft_interrupts: number of software interrupts since boot. Always set to 0 on Windows and SunOS.
-        #syscalls: number of system calls since boot. Always set to 0 on Linux.
         cpu_stats = psutil.cpu_stats()
         print(f'Stats : {cpu_stats}')
 
         cpu_load_avg = psutil.getloadavg()
         print(f'Load AVG : {cpu_load_avg}')
         print(f'Percentage representation : {[x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]} \n')
-
-
-
-
 
 collecting_cpu_informations(5)
