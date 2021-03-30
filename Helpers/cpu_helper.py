ea00e840f10417c06273332aee7901b27c1c48f7
Corentinvls <corentin.vallois@gmail.com>
2021-03-30 14:20:30 +0200

    rework cpuhelper

--- Helpers/cpu_helper.py
+++ Helpers/cpu_helper.py
@@ -5,40 +5,29 @@ by Sarah Hayat
 import psutil
 
 
-def collecting_cpu_informations(times):
+def collecting_cpu_informations():
     """
             Collecting all CPU informations
             :parameter times: number of times you want to execute
             :return: description of cpu
     """
-    for x in range(times):
-        # user: time spent by normal processes executing in user mode; on Linux this also includes guest time
-        # system: time spent by processes executing in kernel mode
-        # idle: time spent doing nothing
 
-        cpu_times = psutil.cpu_times()
-        print(f'Times : {cpu_times}')
+    # user: time spent by normal processes executing in user mode; on Linux this also includes guest time
+    # system: time spent by processes executing in kernel mode
+    # idle: time spent doing nothing
 
-        cpu_percent = psutil.cpu_percent(interval=1, percpu=True)
-        print(f'Percent : {cpu_percent}')
+    # ctx_switches: number of context switches (voluntary + involuntary) since boot.
+    # interrupts: number of interrupts since boot.
 
-        cpu_times_percent = psutil.cpu_times_percent(percpu=True)
-        print(f'Times Percent : {cpu_times_percent}')
-
-        cpu_count = psutil.cpu_count(logical=True)
-        print(f'Count : {cpu_count}')
-
-        cpu_freq = psutil.cpu_freq(percpu=True)
-        print(f'Freq : {cpu_freq}')
-
-        # ctx_switches: number of context switches (voluntary + involuntary) since boot.
-        # interrupts: number of interrupts since boot.
-        cpu_stats = psutil.cpu_stats()
-        print(f'Stats : {cpu_stats}')
-
-        cpu_load_avg = psutil.getloadavg()
-        print(f'Load AVG : {cpu_load_avg}')
-        print(f'Percentage representation : {[x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]} \n')
+    return dict({"times": psutil.cpu_times(),
+                 "percent": psutil.cpu_percent(interval=1, percpu=True),
+                 "time_percent": psutil.cpu_times_percent(percpu=True),
+                 "count": psutil.cpu_count(logical=True),
+                 "freq": psutil.cpu_freq(percpu=True),
+                 "stats": psutil.cpu_stats(),
+                 "load_avg": psutil.getloadavg(),
+                 "percent_representation": [x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]
+                 })
 
 
-collecting_cpu_informations(5)
+print(collecting_cpu_informations())
