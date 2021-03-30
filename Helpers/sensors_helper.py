5c0b1bc8abd47d634fa6ed06180afa7acac9af2b
Corentinvls <corentin.vallois@gmail.com>
2021-03-30 15:53:58 +0200

    all helper done need to merge scripts

--- Helpers/sensors_helper.py
+++ Helpers/sensors_helper.py
@@ -1,66 +1,33 @@
 """
-Module Collecting sensor's Informations
+Module Collecting cpu's Informations
 by Juan-Fernando Herrada
 """
-
-from __future__ import print_function
-
-import sys
-
 import psutil
-from psutil._common import sbattery
 
 
-def secs2hours(secs):
-    mm, ss = divmod(secs, 60)
-    hh, mm = divmod(mm, 60)
-    return "%d:%02d:%02d" % (hh, mm, ss)
-    # '%d:%02d:%02d' means hh:mm:ss
+def get_battery_data():
+    """
+           get data of the battery
 
 
-def main():
-    if hasattr(psutil, "sensor_temperature"):
-        temp = psutil.sensors_temperatures()
-    else:
-        temp = {}
-    if hasattr(psutil, "sensor_battery"):
-        batt = psutil.sensors_battery()
-    else:
-        batt = {}
-    if hasattr(psutil, "sensor_fans"):
-        fans = psutil.sensors_fans()
-    else:
-        fans = {}
+            :return: dictionnary on data
+            :rtype: dict
 
-    if not any((temp, batt, fans)):
-        print("Can not read any information about temperature, battery and fans")
-        return
+            :Example:
 
-    infos = set(list(temp) + list(fans))
-    for info in infos:
-        print(info)
-        # Temperature
-        if info in temp:
-            print("Temperature : ")
-            for entry in temp[info]:
-                print(" %-20s %s°C (high=%s°C, critical=%s°C)" % (entry.label or info, entry.current, entry.high, entry.critical))
-        # Fans
-        if info in fans:
-            print("Fans : ")
-            for entry in fans[info]:
-                print("%-20s %s RPM" % (entry.label or info, entry.current))
+            >>> get_battery_data()
+            {'charge': 82, 'status': 'charging', 'left': 0}
 
-    # Battery
-    print("charge:     %s%%" % round(batt.percent, 2))
-    if batt.power_plugged:
-        print("status:     %s" % (
-            "charging" if batt.percent < 100 else "fully charged"))
-        print("plugged in: yes")
-    else:
-        print("left:       %s" % secs2hours(batt.secsleft))
-        print("status:     %s" % "discharging")
-        print("plugged in: no")
+    """
+    if not hasattr(psutil, "sensors_battery"):
+        return "platform not supported"
+    batt = psutil.sensors_battery()
+    if batt is None:
+        return "no battery is installed"
+
+    return dict({"charge": round(batt.percent, 2),
+                 "status": "charging" if batt.power_plugged else "discharging",
+                 "left": batt.secsleft if type(batt.secsleft) == int else 0})
 
 
-if __name__ == '__main__':
-    main()
+print(get_battery_data())
