34b92f31801a9bfd45357bcc4381e01e0e2f0d15
DebailleuxJonathan <jonathan.debailleux@edu.itescia.fr>
2021-04-01 21:09:32 +0200

    pie chart done

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -17,8 +17,20 @@ def fetch_data():
     return Helper.extract_list_of_value(json.loads(req.content), "charge")
 
 
+def fetch_data_disk():
+    req = requests.get(url=f"http://127.0.0.1:5000/api/?component=disk&time=6h")
+    return json.loads(req.content)
+
+
+df_disk = fetch_data_disk()
+print(df_disk)
+used = Helper.extract_list_of_value(df_disk, "used")
+free = Helper.extract_list_of_value(df_disk, "free")
+all_user_disk = Helper.extract_list_of_host(used)
+
 df = fetch_data()
 all_user = Helper.extract_list_of_host(df)
+
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
@@ -29,32 +41,34 @@ app.layout = html.Div([
         value=all_user,
         labelStyle={'display': 'inline-block'}
     ),
-    dcc.Graph(id="line-chart"),    dcc.Checklist(
-        id="couille",
-        options=[{"label": user, "value": user}
-                 for user in all_user],
-        value=all_user,
-        labelStyle={'display': 'inline-block'}
+    dcc.Graph(id="line-chart"),
+    html.P("Names:"),
+    dcc.Dropdown(
+        id='names',
+        options=[{'value': user, 'label': user}
+                 for user in all_user_disk],
+        value=all_user_disk[0],
+        clearable=False
     ),
-    dcc.Graph(id="chatte")
+    dcc.Graph(id="pie-chart"),
 ])
+
+
+@app.callback(
+    Output("pie-chart", "figure"),
+    [Input("names", "value")])
+def generate_chart(names):
+    usedValue = Helper.extract_data_where_users(used, names)[-1]
+    freeValue = Helper.extract_data_where_users(free, names)[-1]
+    value = {'values': [usedValue.get("used"), freeValue.get("free")], names: ["used", "free"]}
+
+    fig = px.pie(value, values='values', names=names)
+    return fig
 
 
 @app.callback(
     Output("line-chart", "figure"),
     [Input("checklist", "value")])
-def update_line_chart(users):
-    df = fetch_data()
-    display = Helper.extract_data_where_users(df, users)
-    fig = px.line(display,
-                  y="charge", x="time", color='user')
-    return fig
-
-
-
-@app.callback(
-    Output("chatte", "figure"),
-    [Input("couille", "value")])
 def update_line_chart(users):
     df = fetch_data()
     display = Helper.extract_data_where_users(df, users)
